b4e70089af570fbe07b6e05410716f817d700378
Burak Senturk <senturkburk@gmail.com>
2023-12-01 01:14:19 +0300

    Admin Layout oluşturuldu.

---
+++ RestaurantApp.Web/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RestaurantApp.Web.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarComponentPartial : ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
